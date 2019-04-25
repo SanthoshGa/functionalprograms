b10958d9f2ff453b18ab56d3675a822d595e2685
G.Santhosh <49428544+SanthoshGa@users.noreply.github.com>
2019-04-25 09:51:36 +0530

    Update TwoDArray.java

--- functionalprograms/TwoDArray.java
+++ functionalprograms/TwoDArray.java
@@ -2,6 +2,18 @@ package com.bridgelabz.functionalprograms;
 
 public class TwoDArray 
 {
+	public static void main(String[] args) 
+	{
+		try 
+		{
+			Utility.twoDArray();
+		}
+		catch (Exception e) 
+		{
+			// TODO: handle exception
+			System.out.println("Invalid data is entered");
+		}
+	}
 	
 
 }
