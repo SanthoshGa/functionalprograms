e20a087cccbddd4e048bee8b0ebd6c5822f7b013
G.Santhosh <49428544+SanthoshGa@users.noreply.github.com>
2019-04-25 09:14:32 +0530

    Add files via upload

---
+++ functionalprograms/CouponNumber.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.functionalprograms;
+
+import com.bridgelabz.algorithms.Utility;
+
+public class CouponNumber 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("enter the length of coupon");
+		int len = Utility.getNumber();
+		Utility.coupon(len);
+	}
+}
